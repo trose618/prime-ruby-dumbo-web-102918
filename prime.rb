24cf0233e71839a5facfabf0f314b672fff40600
Terrance Rose Jr <trose618@gmail.com>
2018-10-20 20:36:45 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,10 @@
 # Add  code here!
 def prime? (num)
-  if num == 2 return true
+  if num == 1 return false
+  if num == 2 
+    return true
+  else
+    for value in 1..num
+      return false if num % value == 0
+    end
 end

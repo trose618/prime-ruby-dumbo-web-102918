20485ba169ac1b2a89dbdbd6520d5d290f1c5a24
Terrance Rose Jr <trose618@gmail.com>
2018-10-20 20:40:44 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime? (num)
+  return false if num <= 1
+  if num == 2 
+    return true
+  else
+    for value in 2...num
+      return false if num % value == 0
+    end
+  end
+    true
+end
